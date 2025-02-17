511cc706f6cb1524c15933fbe4d7bf3e789bc11e
Liset <lisetbeatriz.armas@gmail.com>
2025-02-17 11:18:32 +0100

    Update dependencies version and refactor DriverSetup class by replacing AppiumDriver with AndroidDriver

---
+++ src/main/java/org/example/utils/DriverSetup.java
@@ -0,0 +1,47 @@
+package org.example.utils;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.net.URL;
+
+public class DriverSetup {
+    protected AndroidDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        if (driver == null) {
+            try {
+                ConfigLoader configLoader = new ConfigLoader();
+                DesiredCapabilities capabilities = getCapabilities(configLoader);
+                driver = new AndroidDriver(new URL(configLoader.get("serverUrl")), capabilities);  // Use AndroidDriver
+            } catch (Exception e) {
+                throw new RuntimeException("Error during driver setup: " + e.getMessage(), e);
+            }
+        }
+    }
+
+
+    private DesiredCapabilities getCapabilities(ConfigLoader configLoader) {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", configLoader.get("platformName"));
+        capabilities.setCapability("platformVersion", configLoader.get("platformVersion"));
+        capabilities.setCapability("deviceName", configLoader.get("deviceName"));
+        capabilities.setCapability("udid", configLoader.get("udid"));
+        capabilities.setCapability("automationName", configLoader.get("automationName"));
+        capabilities.setCapability("app", configLoader.get("appPath"));
+        capabilities.setCapability("appActivity", configLoader.get("appActivity"));
+        capabilities.setCapability("appWaitActivity", configLoader.get("appWaitActivity"));
+
+        return capabilities;
+    }
+
+    @AfterTest
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
