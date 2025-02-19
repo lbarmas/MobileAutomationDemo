ef9e950173413c1e417b868e3a7a303e07a4c27f
Liset <lisetbeatriz.armas@gmail.com>
2025-02-19 09:36:59 +0100

    Add Cucumber step definitions in LoginAndAddProductsSteps class

--- src/test/java/stepdefinitions/LoginAndAddProductsSteps.java
+++ src/test/java/stepdefinitions/LoginAndAddProductsSteps.java
@@ -4,36 +4,44 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.example.pages.LoginAndAddProductsPage;
+import org.example.utils.DriverSetup;
+import org.junit.Assert;
 
-public class LoginAndAddProductsSteps {
+public class LoginAndAddProductsSteps extends DriverSetup {
+
+    private LoginAndAddProductsPage loginAndAddProductsPage;
 
     @Given("the user opens the SwagLabs app")
     public void theUserIsOnTheLoginPage() {
-
+        setup();
+        loginAndAddProductsPage = new LoginAndAddProductsPage(driver);
     }
 
     @When("the user enters a valid username {string} and password {string}")
     public void enterValidUserNameAndPassword(String username, String password){
-
+        loginAndAddProductsPage.enterUsername(username);
+        loginAndAddProductsPage.enterPassword(password);
     }
 
     @And("the user clicks the login button")
     public void clickOnTheLoginButton(){
-
+        loginAndAddProductsPage.clickLoginButton();
+        loginAndAddProductsPage = new LoginAndAddProductsPage(driver);
     }
 
     @And("the app navigates to the home screen")
     public void appNavigatesToTheHomeScreen(){
-
+        Assert.assertTrue(loginAndAddProductsPage.isHomeTitleDisplayed());
     }
 
     @And("the user adds products to the cart")
     public void theUserAddsProductToTheCart() {
-
+        loginAndAddProductsPage.selectProduct();
     }
 
     @Then("the products are added to the cart successfully")
     public void theProductIsAddedToTheCartSuccessfully(){
-
+       Assert.assertTrue(loginAndAddProductsPage.isProductsAddedToCart());
     }
 }
