9df81e03427329c8da3f967c100ced1b697ca2d1
Liset <lisetbeatriz.armas@gmail.com>
2025-03-04 16:53:41 +0100

    Update features path and tags to match the correct ID

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -3,12 +3,12 @@ package runners;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = "src/test/resources/features", glue = "org.example.stepdefinitions",
+@CucumberOptions(features = "src/test/java/features", glue = "stepdefinitions",
         plugin = {"pretty",
                 "html:target/cucumber-reports.html",
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
         monochrome = true,
-        tags = "@smoke")
+        tags = "@add_to_cart")
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 
